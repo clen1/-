7c864e7b3c8d3fa9d1266627195daef7bffcdeca
Elik <3440936472@qq.com>
2023-11-21 16:47:50 +0800

    初始小程序

---
+++ miniprogram-3/app.js
@@ -0,0 +1,9 @@
+// app.js
+App({
+  
+  globalData: {
+    lotterySoundSrc: '/pages/music/result.mp3',
+    resultSoundSrc: '/pages/music/shake.mp3',
+    // imagePath: '/pages/images/book.webp',
+  }
+})
